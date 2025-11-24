1ee92398f894bc22c6c37f9fd59cea3d8835e566
Gustavo <gustavo.alarcao@gmail.com>
2025-11-23 23:37:09 -0300

    Ready for deployment

---
+++ app_counter.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+
+st.set_page_config(layout='centered')
+
+st.markdown("""
+    <style>
+    div.stButton > button {
+        height: 80px;  /* Tall buttons for fat fingers */
+        font-weight: bold;
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
+
+st.title('Contar chegadas')
+
+if 'df' not in st.session_state:
+    df = pd.DataFrame({
+        'Tempo_chegada': []
+    })
+    st.session_state.df = df
+
+with st.container(border=True):
+    cols = st.columns(2)
+    with cols[0]:
+        for i in range(1, 11, 2):
+            st.button(
+                label=f'+{i}',
+                width='stretch',
+                type='primary',
+                key=f'b{i}'
+            )
+    with cols[1]:
+        for i in range(2, 11, 2):
+            st.button(
+                label=f'+{i}',
+                width='stretch',
+                type='primary',
+                key=f'b{i}'
+            )
+for i in range(1, 11):
+    if st.session_state[f'b{i}']:
+        new_row = pd.DataFrame({'Tempo_chegada': i*[datetime.now().strftime("%H:%M:%S")]})
+        st.session_state.df = pd.concat([st.session_state.df, new_row])
+        st.toast(
+            body='salvo!',
+            duration=1,
+            icon='✅'
+        )
+
+csv = st.session_state.df.to_csv(index=False).encode('utf-8')
+st.download_button(
+    "📥Baixar",
+    csv,
+    f"contagem.csv",
+    "text/csv",
+    use_container_width=True
+)
+    
+
+        
+        
+
+    
